0c088d86801cebef28bcf86c33503e863c57331a
Vincent <vpieplu@gmail.com>
2019-05-16 17:40:04 +0200

    Translate to FR

--- resources/js/Index/Index.vue
+++ resources/js/Index/Index.vue
@@ -6,26 +6,26 @@
 
     <div class="flex mb-6">
 
-      <checkbox-with-label class="mr-6" @change="changeBulk" :checked="bulk.is" v-if="tool || gallery">Bulk select</checkbox-with-label>
+      <checkbox-with-label class="mr-6" @change="changeBulk" :checked="bulk.is" v-if="tool || gallery">Sélection</checkbox-with-label>
 
       <button type="button" v-if="bulk.is" @click="bulkAll"
               class="form-file-btn btn-default btn-primary cursor-pointer shadow-md mr-6">
-        Select all
+        Sélectionner tout
       </button>
 
       <button type="button" v-if="bulk.is && bulk.array.length" @click="deleteFiles(bulk.array)"
               class="form-file-btn btn-default btn-danger cursor-pointer shadow-md">
-        Delete selected: {{ bulk.array.length }}
+        Supprimer la sélection : {{ bulk.array.length }}
       </button>
 
       <button type="button" v-if="!tool && bulk.is && bulk.array.length" @click="pushFiles"
               class="form-file-btn btn-default text-white bg-success cursor-pointer shadow-md ml-6">
-        Add to Gallery: {{ bulk.array.length }}
+        Ajouter à la galerie : {{ bulk.array.length }}
       </button>
 
       <label class="form-file-btn btn btn-default btn-primary cursor-pointer shadow-md ml-auto">
         <input id="nml_upload" class="form-file-input" :accept="types" type="file" multiple @change="selectFiles" />
-        Upload Files
+        Ajouter des photos
       </label>
     </div>
 
@@ -35,15 +35,8 @@
       <div class="relative mr-4">
         <icon type="search" class="absolute search-icon-center ml-3 text-70" />
         <input class="form-control form-input w-search pl-search shadow-md" type="search"
-               placeholder="Search by Title" v-model="filter.title" @input="doSearch()"/>
+               placeholder="Rechercher par le titre" v-model="filter.title" @input="doSearch()"/>
       </div>
-
-      <date-time-picker class="form-control form-input shadow-md mr-4" autocomplete="off" placeholder="Upload date from"
-                        dateFormat="Y-m-d" :firstDayOfWeek="1" :enable-time="false" @change="val => updateDate(val, 'from')" />
-
-      <date-time-picker class="form-control form-input shadow-md mr-4" autocomplete="off" placeholder="Upload date to"
-                        dateFormat="Y-m-d" :firstDayOfWeek="1" :enable-time="false" @change="val => updateDate(val, 'to')" />
-
     </div>
 
     <!-- Items -->
@@ -62,10 +55,10 @@
 
     <template v-if="!loading && !data.full">
       <button type="button" @click="loader" class="form-file-btn btn-default btn-primary cursor-pointer mt-8 m-auto block shadow-md">
-        {{ tool ? 'Click if auto load is not working' : 'Load More' }}
+        {{ tool ? 'Cliquez si le chargement automatique ne fonctionne pas' : 'Voir plus' }}
       </button>
       <div v-if="tool" class="help-text help-text pb-4 mt-2 m-auto block text-center">
-        Scroll down to automatically download files or click the button above
+        Faites défiler vers le bas pour télécharger automatiquement les fichiers ou cliquez sur le bouton ci-dessus
       </div>
     </template>
 
